73b9e1505033a194f65778ecd85c1ac51f812741
Toad Bundy <ChumBucketSurfer@gmail.com>
2022-07-15 12:20:52 +0300

    added some changes

--- Ablazzing_homeworks/src/package_1/homework1.java
+++ Ablazzing_homeworks/src/package_1/homework1.java
@@ -7,22 +7,23 @@ import java.util.Arrays;
  */
 public class homework1 {
     public static void main(String[] args) {
-        //Базовый уровень
-        //Задача №1
+            //Базовый уровень
+            //Задача №1
         System.out.println("********************************");
         System.out.println("Base level task 1 ");
         System.out.println("********************************");
-        //Дано (переменные ниже менять нельзя)
+            //Дано (переменные ниже менять нельзя)
         String hi = "                Hello ";
         String world = " WoRld!";
         char newLine = '\n';
-        //Создать из трех переменных единую строку,
+            //Создать из трех переменных единую строку,
+
         String mystr = hi + world + newLine;
         System.out.print(mystr);
-        //Привести к правильному виду (Ниже) используя String.trim()
+            //Привести к правильному виду (Ниже) используя String.trim()
         mystr = hi.trim() + world.toLowerCase()+newLine;
         System.out.print(mystr);
-        //затроить (Можно вызвать тольку одну команду System.out.printl n())
+            //затроить (Можно вызвать тольку одну команду System.out.printl n())
         System.out.println();
         System.out.print(mystr.repeat(3));
         //
@@ -32,27 +33,27 @@ public class homework1 {
         //Hello world!
 
 
-        //Задача №2
+            //Задача №2
         System.out.println("******************************");
         System.out.println("Base level task 2");
         System.out.println("******************************");
-        //Создать переменные с ростом (!в метрах), весом в кило.
+            //Создать переменные с ростом (!в метрах), весом в кило.
         float height = 1.83f;
         float weight = 74.8f;
         float index;
-        //Произвести расчет индекса массы тела (вес) / (рост * рост) используя переменные, вывести на экран
+            //Произвести расчет индекса массы тела (вес) / (рост * рост) используя переменные, вывести на экран
         index = weight/(height * height);
-        //Пример результата вывода на экран:
+            //Пример результата вывода на экран:
         System.out.println("height = "+height + ", weight= "+weight);
         System.out.println("index = " + index);
         //21.0345645
 
-        //Задача №3
+            //Задача №3
         System.out.println("******************************");
         System.out.println("Base level task 3");
         System.out.println("******************************");
-        //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
-        //и снова создать строку, вывести на экран
+            //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
+            //и снова создать строку, вывести на экран
         char [] array = new char[]{'a','b','c','d','e'};
         String str = new String(array);
         System.out.println(str);
@@ -64,17 +65,18 @@ public class homework1 {
         //abche
 
 
-        //Продвинутый уровень
-        //Задача №1
+            //Продвинутый уровень
+            //Задача №1
         System.out.println("*******************");
         System.out.println("Advanced level task 1");
         System.out.println("*******************");
-        //Произвести преобразование текста "234" в число типа int и прибавить к этому числу длину строки "some_text"
+            //Произвести преобразование текста "234" в число типа int и прибавить к этому числу длину строки "some_text"
         str = "234";
         String text = "some_text";
         int num = Integer.parseInt(str);
         System.out.println(num + text.length());
-        //Задача №2
+            //Задача №2
+            //Посчитать (a+b)^2 = ?, при a=3, b=5
         System.out.println("********************");
         System.out.println("Advanced level task 2");
         System.out.println("********************");
@@ -82,9 +84,9 @@ public class homework1 {
         double b = 5;
         double c = Math.pow((a+b), 2);
         System.out.println(c);
-        //Посчитать (a+b)^2 = ?, при a=3, b=5
 
-        //Задача №3
+
+            //Задача №3
         System.out.println("********************");
         System.out.println("Advanced level task 3");
         System.out.println("********************");
@@ -94,8 +96,8 @@ public class homework1 {
         int[] array_num1 = new int[] {1,2,5,7,10};
         int[] array_num2 = new int[] {2,3,2,17,15};
         int[] result = new int[3*array_num1.length];
-        // Создать массив чисел, в котором будут: все числа из этих двух массивов,
-        // и результат умножения чисел с одинаковым порядковым номером
+            // Создать массив чисел, в котором будут: все числа из этих двух массивов,
+            // и результат умножения чисел с одинаковым порядковым номером
         System.arraycopy(array_num1,0,result, 0, array_num1.length);
         System.arraycopy(array_num2,0,result, array_num1.length, array_num2.length);
         result[10] = array_num1[0]*array_num2[0];
@@ -104,35 +106,57 @@ public class homework1 {
         result[13] = array_num1[3]*array_num2[3];
         result[14] = array_num1[4]*array_num2[4];
         System.out.println(Arrays.toString(result));
+        System.out.println("********************");
+        System.out.println("Advanced level task 3(variant 2)");
+        for (int i = 0; i< array_num1.length; i++){
+            result[i] = array_num1[i];
+            result[i+5] = array_num2[i];
+            result[i+10] = array_num1[i]*array_num2[i];
+        }
+        System.out.println(Arrays.toString(result));
         //
         //Ожидаемый результат:
         //1,2,5,7,10,2,3,2,17,15,2,6,10,119,150
         //(первый массив - 1,2,5,7,10), (второй массив - 2,3,2,17,15),
         //(результат перемножения - (1*2), (2*3), (5*2), (7*17), (10*15)
 
-        //Задача №4
+            //Задача №4
         System.out.println("********************");
         System.out.println("Advanced level task 4");
         System.out.println("********************");
-        //В слове "Hello world!" заменить l на r, сделать все буквы заглавными, выбрать первые 8 символов, вывести на экран
+            //В слове "Hello world!" заменить l на r, сделать все буквы заглавными, выбрать первые 8 символов, вывести на экран
         String line = "Hello world!";
         line =  line.replaceAll("l", "r");
         line = line.toUpperCase();
         System.out.println(line.substring(0,8));
 
-        //Экспертный уровень
+            //Экспертный уровень
         System.out.println("********************");
         System.out.println("Expert level task 1");
         System.out.println("********************");
-        //Задача №1
-        //Создать метод маскирования персональных данных, который:
-        //Телефон (до/после маскирования): 79991113344 / 7999***3344
-        //Email (до/после маскирования): test@yandex.ru / tes*@******.ru, my_mail@gmail.com / my_mai*@*****.com
-        //Фио (до/после маскирования): Иванов Иван Иванович / И****в Иван И.
-        //
-        //Входящие параметры: String text
-        //Возвращаемый результат: String
+            //Задача №1
+            //Создать метод маскирования персональных данных, который:
+            //Телефон (до/после маскирования): 79991113344 / 7999***3344
+            //Email (до/после маскирования): test@yandex.ru / tes*@******.ru, my_mail@gmail.com / my_mai*@*****.com
+            //Фио (до/после маскирования): Иванов Иван Иванович / И****в Иван И.
+            //
+            //Входящие параметры: String text
+            //Возвращаемый результат: String
         String js_text = "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>";
+
+        mask(js_text);
+
+        //Примеры возможного текста:
+        //Входящий стринг <client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>
+        //Вернет - "<client>(Какие то данные)<data>7999***3344;tes*@******.ru;И****в Иван И.</data></client>"
+        //<client>(Какие то данные)<data></data></client> - вернет тоже (никаких ошибок)
+        //Входящий - <client>(Какие то данные)<data>Иванов Иван Иванович;79991113344</data></client>
+        //Вернет - "<client>(Какие то данные)<data>И****в Иван И.;7999***3344</data></client>"
+
+        //Используемые технологии: String.find, String.replaceAll, String.split, String.join, String.contains, String.substring
+        //Регулярные выражения, класс StringBuilder
+    }
+    public static void mask(String js_text){
         int start = js_text.indexOf("<data>");
         int fin = js_text.indexOf("</data>");
         String data = js_text.substring(start+6, fin);
@@ -153,15 +177,5 @@ public class homework1 {
         System.out.println(phone_number);
         System.out.println(email);
         System.out.println(name);
-
-        //Примеры возможного текста:
-        //Входящий стринг <client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>
-        //Вернет - "<client>(Какие то данные)<data>7999***3344;tes*@******.ru;И****в Иван И.</data></client>"
-        //<client>(Какие то данные)<data></data></client> - вернет тоже (никаких ошибок)
-        //Входящий - <client>(Какие то данные)<data>Иванов Иван Иванович;79991113344</data></client>
-        //Вернет - "<client>(Какие то данные)<data>И****в Иван И.;7999***3344</data></client>"
-
-        //Используемые технологии: String.find, String.replaceAll, String.split, String.join, String.contains, String.substring
-        //Регулярные выражения, класс StringBuilder
     }
 }
